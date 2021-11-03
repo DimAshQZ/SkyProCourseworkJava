b3425ae23b12d5a7bdca4849b71fd8a2509ae5de
D!WV$ <sovetbekovds@gmail.com>
2021-11-03 18:55:32 +0600

    Завершил работу с классом Employee для базовой сложности

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -1,8 +1,65 @@
 package com.company;
 
+import java.util.Objects;
+
 public class Employee {
-    private String FIO;
-    private byte Otdel;
-    private int ZP;
-    static int id;
+    public static int count;
+    private final String name;
+    private int department;
+    private double salary;
+    private int id;
+
+    public Employee(String name, int department, int salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+        this.id = ++count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                ", id=" + id +
+                '}';
+    }
 }
